6c0ef3837f3298d08974a8c72e4dcc9113261993
sserdardundar <serdardunda976@gmail.com>
2025-03-21 11:45:54 +0300

    update content.js

--- content.js
+++ content.js
@@ -1,23 +1,47 @@
-function isGitHubRepo() {
-  return document.location.pathname.split("/").length >= 3;
+function isRepoDirectory() {
+  const pathSegments = window.location.pathname.split("/");
+  return pathSegments.length >= 4 && pathSegments[3] === "tree";
+}
+
+function constructDownloadURL() {
+  const repoURL = window.location.href;
+  return `https://download-directory.github.io/?url=${repoURL}`;
 }
 
 function insertDownloadButton() {
-  if (!isGitHubRepo()) return;
+  if (!isRepoDirectory()) return;
 
-  if (document.querySelector("#download-repo-button")) return;
+  if (document.querySelector("#download-directory-button")) return;
 
-  const actionsBar = document.querySelector(".file-navigation");
-  if (!actionsBar) return;
+  const container = document.querySelector(".file-navigation");
+  if (!container) return;
 
-  let button = document.createElement("a");
-  button.id = "download-repo-button";
-  button.innerText = "Download Repo";
-  button.href = `https://download-directory.github.io/${document.location.pathname}`;
-  button.target = "_blank";
+  const button = document.createElement("a");
+  button.id = "download-directory-button";
+  button.innerText = "Download Directory";
+  button.href = "#";
   button.className = "btn btn-primary";
 
-  actionsBar.appendChild(button);
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const downloadURL = constructDownloadURL();
+    fetch(downloadURL)
+      .then((response) => {
+        if (response.ok) {
+          const tempLink = document.createElement("a");
+          tempLink.href = downloadURL;
+          tempLink.download = "";
+          document.body.appendChild(tempLink);
+          tempLink.click();
+          document.body.removeChild(tempLink);
+        } else {
+          console.error("Failed to initiate download.");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  });
+
+  container.appendChild(button);
 }
 
-window.onload = insertDownloadButton;
+window.addEventListener("load", insertDownloadButton);
